7bc98aae1940e4d896f5a759bbdfb1fb79b31c9c
Alex-primo <60712192+Alex-primo@users.noreply.github.com>
2022-03-17 18:11:52 -0400

    Backend for Landingpage | had to change file directories for flask

--- app.py
+++ app.py
@@ -21,42 +21,6 @@ mysql.init_app(app)
 
 connection = mysql.connect()
 
-# insert email / pwd / username into the users table
-"""
-def mySQL_insert(email, pwd, username):
-    cur = mysql.connection.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS users(email VARCHAR(255), pwd VARCHAR(255), username VARCHAR(255))")
-    cur.execute("INSERT INTO users(email, pwd, username) VALUES (%s, %s, %s)", (email, pwd, username))
-    mysql.connection.commit()
-    cur.close()
-"""
-
-# check the users pass to see if it matches
-# returns True or False
-"""
-def mySQL_check_pass(user, pwd):
-    cur = mysql.connection.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS users(email VARCHAR(255), pwd VARCHAR(255), username VARCHAR(255))")
-    cur.execute("SELECT * FROM users WHERE username = '"+user+"'")
-    rows = cur.fetchall()
-    mysql.connection.commit()
-    cur.close()
-    return rows[0][1] == pwd
-"""
-
-# check if username / email exists already in the database
-# returns True of False
-"""
-def mySQL_check_key_exists(key, value):
-    cur = mysql.connection.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS users(email VARCHAR(255), pwd VARCHAR(255), username VARCHAR(255))")
-    cur.execute("SELECT * FROM users WHERE "+key+" = '"+value+"'")
-    rows = cur.fetchall()
-    mysql.connection.commit()
-    cur.close()
-    return len(rows) > 0
-"""
-
 #-------------------------------------
 #           -FLASK ROUTING-
 #-------------------------------------
@@ -65,7 +29,6 @@ def home():
     return render_template('index.html')
 
 #######################
-
 
 @app.route('/login.html', methods=['GET'])
 def login():
@@ -82,6 +45,7 @@ def login_user():
     u_password = request.form['pwd']
     cursor = connection.cursor()
 
+    cursor.execute("CREATE TABLE IF NOT EXISTS users(email VARCHAR(255), pwd VARCHAR(255), username VARCHAR(255))")
     cursor.execute("SELECT * from users where username = %s AND PWD = %s", (u_username, u_password))
     connection.commit()
     result = cursor.fetchall()
@@ -91,10 +55,9 @@ def login_user():
         return render_template('login.html')
     else:
         session['username'] = u_username
-        return redirect(url_for('home', session['username']))
+        return redirect(url_for('landingPage', username=session['username']))
 
-    
-
+###########################
 
 @app.route('/logout.html')
 def logout_user():
@@ -103,13 +66,11 @@ def logout_user():
 
     return redirect(url_for('home'))
 
-
 ###########################
 
 @app.route('/register.html', methods=['GET'])
 def register():
     return render_template('register.html')
-
 
 #############################
 
@@ -119,19 +80,21 @@ def create_user():
     u_username = request.form['uname']
     u_password = request.form['pwd']
 
-
     cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS users(email VARCHAR(255), pwd VARCHAR(255), username VARCHAR(255))")
     cursor.execute("INSERT INTO users(email, pwd, username) VALUES (%s, %s, %s)", (u_email, u_password, u_username))
     connection.commit()
-
-    #if not (mySQL_check_key_exists("username",u_username)):
-    #    mySQL_insert(u_email, u_password, u_username)
-    #else:
-    #    return 'User already exists!' #DO NOT ALLOW TO REGISTER, USERNAME ALREADY EXISTS
     flash("User successfully added!") #SUCCESSFULLY REGISTER
     return redirect(url_for('login'))
 
+#############################
 
+@app.route('/landingPage/index.html', methods=['GET'])
+def landingPage():
+    return render_template('landingPage/index.html')
+
+
+#############################
 
 if __name__ == '__main__':
     #app.run(host='128.205.32.39', port=7321)
---
+++ static/styles/landingPage/javascript/main.js
@@ -0,0 +1,88 @@
+// Use a test username until the actual user is retrieved from the server.
+var USERNAME="TestUser"
+var TOKEN="5788ygGUYGUG86g5r7KGHJHB"
+// The default recipient (and corresponding picture) is the Shoutbox.
+var RECIPIENT="Shoutbox"
+var RECIPIENT_PICTURE="/static/styles/landingPage/images/crown_a.png"
+
+// A template for the message html. This template can be inserted
+// into the chatbox in order to render a new message.
+function gen_message_template(sender, message) {
+	return `
+		<div class="message">
+			<div class="message-sender">${sender}</div>
+			<div class="message-text">${message}</div>
+		</div>
+		<hr/>
+	`;
+}
+
+// Populates the default information when the page loads.
+function on_load() {
+	const params = new URLSearchParams(window.location.search);
+	USERNAME = params.get("username");
+
+	populate_values()
+}
+
+// The purpose of this function is to set all of the different values
+// on the page. It is expected that when this is called, all global
+// variables have their proper values.
+function populate_values() {
+	// Set the profile name
+	profile_name_element = document.getElementById('profile-name');
+	profile_name_element.innerHTML = USERNAME;
+
+	// Set the recipient name
+	recipient_name_element = document.getElementById('recipient-name');
+	recipient_name_element.innerHTML = RECIPIENT;
+
+	// Set the recipient picture
+	recipient_profile_picture_element = document.getElementById('recipient-photo-image');
+	recipient_profile_picture_element.src = RECIPIENT_PICTURE;
+
+	clear_messages();
+
+	// Indicate that messages recieved from the server will go here
+	recieve_message("*** Load Messages From Server For " + RECIPIENT + " ***");
+}
+
+// Deletes all child elements of the message box, clearing it.
+function clear_messages() {
+	message_frame = document.getElementById('message-frame');
+	while (message_frame.hasChildNodes()) {
+		    message_frame.removeChild(message_frame.lastChild);
+	};
+}
+
+// Placeholder fuction for the logout button.
+function logout() {
+	alert("Logged out");
+}
+
+// Send a message to the server.
+function send_message() {
+	console.log("Send message");
+	var message = document.getElementById("input-field").value;
+	// AJAX Send message
+	recieve_message(message);
+}
+
+// Recieve a message from the server.
+function recieve_message(message) {
+	// AJAX Get message
+	message_frame = document.getElementById('message-frame');
+	message_frame.insertAdjacentHTML('beforeend', gen_message_template(USERNAME, message));
+}
+
+// The "onclick" event when selecting a friend. Based on which friend is selected, that friend's
+// name and the link to their profile picture is stored, then the site repopulated.
+function select_friend(object) {
+	// In every "friend" div there is only one object with the 'friend-name' and one object
+	// with the 'friend-photo-image' class tag. The clicked "friend" div is passed in so we
+	// can just find the first child of the given classes, since there should only be one
+	// of each.
+	RECIPIENT = object.getElementsByClassName('friend-name')[0].innerHTML;
+	RECIPIENT_PICTURE = object.getElementsByClassName('friend-photo-image')[0].src;
+	populate_values();
+}
